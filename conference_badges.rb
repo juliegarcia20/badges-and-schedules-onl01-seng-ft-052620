43e1e622cd21a6117b60e2ffdaa1e8389aabf3ea
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 19:44:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,10 @@ def batch_badge_creator(names)
     badge_maker(name)
   end 
 end 
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end 
+end 
+
